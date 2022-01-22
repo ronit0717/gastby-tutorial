89a1a8d41b563fca4af457f53614da438ea93b96
Ronit Chattopadhyay <ronit0717@gmail.com>
2022-01-22 09:00:46 +0530

    Pages

---
+++ src/pages/company/history.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const history = () => {
+  return <div>This is history of our company</div>
+}
+
+export default history
---
+++ src/pages/contact.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function contact() {
+  return <div>This is a contact page</div>
+}
