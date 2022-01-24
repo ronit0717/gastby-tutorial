022a3ab2a8c20a9b150838f2da875d7a6a17b2cc
Ronit Chattopadhyay <ronit0717@gmail.com>
2022-01-24 07:35:14 +0530

    CSS Modules

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,24 @@
 import React from "react"
 import Layout from "../components/Layout"
+import * as aboutStyles from "../examples/about.module.css"
 
 const about = () => {
   return (
     <Layout>
-      <div className="about-heading">Hello its about page</div>
+      <div className={aboutStyles.page}>
+        <h1>Hello its about page</h1>
+        <p className={aboutStyles.text}>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </p>
+      </div>
     </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,27 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
+import { page, text } from "../examples/home.module.css"
 
 export default function Home() {
   return (
     <Layout>
-      <div className="home-heading">
+      <div className={page}>
         <h1>Hello world </h1>
         <h1>Ronit</h1>
       </div>
+
+      <p className={text}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </p>
 
       <div>
         <Link to="/about">About Us</Link>
