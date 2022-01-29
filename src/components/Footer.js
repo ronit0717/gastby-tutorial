420bf38972af4826c53eb5a2e2176890d786c576
Ronit Chattopadhyay <ronit0717@gmail.com>
2022-01-29 11:18:14 +0530

    Footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,14 @@
 import React from "react"
 
 const Footer = () => {
-  return <footer>Hello from Footer</footer>
+  return (
+    <footer className="page-footer">
+      <p>
+        &copy; {new Date().getFullYear()} <span>Simple Receipes</span> Built
+        with <a href="https://www.gatsbyjs.com">Gatsby</a>
+      </p>
+    </footer>
+  )
 }
 
 export default Footer
