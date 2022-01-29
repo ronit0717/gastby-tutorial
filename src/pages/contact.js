238ebad46327edc65b39fe4f09f38dc1db5a74c0
Ronit Chattopadhyay <ronit0717@gmail.com>
2022-01-29 11:34:25 +0530

    Contact Us Page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,41 @@ import Layout from "../components/Layout"
 const Contact = () => {
   return (
     <Layout>
-      <h1>Contact Page</h1>
+      <main className="page">
+        <section className="contact-page">
+          <article className="contact-info">
+            <h3>Want to get in touch?</h3>
+            <p>
+              Four dollar toast biodiesel plaid salvia actually pickled banjo
+              bespoke mlkshk intelligentsia edison bulb synth.
+            </p>
+            <p>Cardigan prism bicycle rights put a bird on it deep v.</p>
+            <p>
+              Hashtag swag health goth air plant, raclette listicle fingerstache
+              cold-pressed fanny pack bicycle rights cardigan poke.
+            </p>
+          </article>
+          <article>
+            <form className="form contact-form">
+              <div className="form-row">
+                <label htmlFor="name">Your name</label>
+                <input type="text" name="name" id="name" />
+              </div>
+              <div className="form-row">
+                <label htmlFor="email">Your email</label>
+                <input type="text" name="email" id="email" />
+              </div>
+              <div className="form-row">
+                <label htmlFor="message">Message</label>
+                <textarea name="message" id="message" />
+              </div>
+              <button type="submit" className="btn block">
+                Submit
+              </button>
+            </form>
+          </article>
+        </section>
+      </main>
     </Layout>
   )
 }
