0a86902344c1a9f2ff46962f67fcfca1482554f3
Ronit Chattopadhyay <ronit0717@gmail.com>
2022-01-29 11:19:58 +0530

    Error Page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,12 @@ import Layout from "../components/Layout"
 const Error = () => {
   return (
     <Layout>
-      <h1>Error Page</h1>
+      <main className="error-page">
+        <section>
+          <h1>404</h1>
+          <h3>Page not found</h3>
+        </section>
+      </main>
     </Layout>
   )
 }
