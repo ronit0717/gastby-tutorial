9745ed738a7867aaf86bfa3de8d6e537e86eb07b
Ronit Chattopadhyay <ronit0717@gmail.com>
2022-01-30 08:56:47 +0530

    Static image all layouts

--- src/examples/Images.js
+++ src/examples/Images.js
@@ -10,9 +10,31 @@ const Images = () => {
         <StaticImage
           src="../assets/images/recipe-1.jpeg"
           alt="food"
-          height={400}
           placeholder="tracedSVG"
           layout="constrained"
+          className="example-img"
+          as="section"
+        />
+      </article>
+      <article>
+        <h4>Fixed</h4>
+        <StaticImage
+          src="../assets/images/recipe-1.jpeg"
+          alt="food"
+          placeholder="blurred"
+          layout="fixed"
+          width={200}
+          className="example-img"
+          as="section"
+        />
+      </article>
+      <article>
+        <h4>Full Width</h4>
+        <StaticImage
+          src="../assets/images/recipe-1.jpeg"
+          alt="food"
+          placeholder="dominantColor"
+          layout="fullWidth"
           className="example-img"
           as="section"
         />
@@ -24,11 +46,21 @@ const Images = () => {
 const Wrapper = styled.section`
   width: 70vw;
   margin: 0 auto;
+  display: grid;
+  text-align: center;
+  gap: 2rem;
   article {
     border: 2px solid red;
   }
   .example-img {
     border-radius: 1rem;
+    height: 300px;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr 1fr;
+    .example-img {
+      height: 200px;
+    }
   }
 `
 
