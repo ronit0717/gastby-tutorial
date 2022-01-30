89f13028bd9a9def8c6504ab04f6182d21cb9768
Ronit Chattopadhyay <ronit0717@gmail.com>
2022-01-30 08:43:56 +0530

    Shared props and images

--- src/examples/Images.js
+++ src/examples/Images.js
@@ -1,20 +1,34 @@
 import React from "react"
 import styled from "styled-components"
-import big from "../assets/images/big.jpg"
 import { StaticImage } from "gatsby-plugin-image"
 
 const Images = () => {
   return (
     <Wrapper>
-      <StaticImage src="../assets/images/big.jpg" alt="food" />
-      <h2>Gatsby Image</h2>
+      <article>
+        <h4>Constrained / default</h4>
+        <StaticImage
+          src="../assets/images/recipe-1.jpeg"
+          alt="food"
+          height={400}
+          placeholder="tracedSVG"
+          layout="constrained"
+          className="example-img"
+          as="section"
+        />
+      </article>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.section`
-  img {
-    width: 200px;
+  width: 70vw;
+  margin: 0 auto;
+  article {
+    border: 2px solid red;
+  }
+  .example-img {
+    border-radius: 1rem;
   }
 `
 
